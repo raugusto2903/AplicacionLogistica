10ed13aaed7888563c14416493c7ef5f1c2013a9
raugusto2903 <109769046+raugusto2903@users.noreply.github.com>
2023-08-29 17:16:11 -0500

    Add files via upload

---
+++ Data/ConexionBD.cs
@@ -0,0 +1,12 @@
+﻿namespace AplcacionLogistica.Data
+{
+    public class ConexionBD
+    {
+        private string cadenaConexionSQL;
+        public string CadenaConexionSql { get => cadenaConexionSQL; }
+        public ConexionBD(string ConexionSql)
+        {
+            cadenaConexionSQL = ConexionSql;
+        }
+    }
+}
